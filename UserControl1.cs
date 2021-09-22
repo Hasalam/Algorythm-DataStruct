f305778edc1bd7093782bb63ce29f829f3db42c9
Hasalam <markshykh@gmail.com>
2021-09-22 20:29:24 +0300

    Deleted unneseccary func

--- UserControl1.cs
+++ UserControl1.cs
@@ -19,13 +19,7 @@ namespace Lab_1_Alg
             InitializeComponent();
         }
 
-        static float NextFloat(Random random)
-        {
-            double mantissa = (random.NextDouble() * 2.0) - 1.0;
-            // choose -149 instead of -126 to also generate subnormal floats (*)
-            double exponent = Math.Pow(2.0, random.Next(-126, 128));
-            return (float)(mantissa * exponent);
-        }
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
